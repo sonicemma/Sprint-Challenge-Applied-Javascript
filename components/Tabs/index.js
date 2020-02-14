cc5fbdd23060bd4a794517e7dcee1d42e67c13fa
emma <sonicjane2020@gmail.com>
2020-02-14 08:38:00 -0800

    Finished mvp under tabs in index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector('.topics');
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics").then(response => {
+    let data = response.data.topics;
+    console.log(data);
+
+    data.forEach(element => {
+        const tab = document.createElement("div");
+
+        topics.appendChild(tab);
+
+        tab.classList.add("tab");
+        
+        tab.textContent = element;
+    });
+
+});
+
+const topicTab = () => {
+    const tab = document.createElement('div')
+
+    tab.classList.add('tab');
+    
+    tab.textContent = 'topic here';
+
+    return tab;
+}
